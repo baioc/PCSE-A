5f1da57f92c4f252ab11e00e4878a7c4438e6634
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-02-12 08:42:26 +0100

    [PROCESS] Added prototypes from header file into source

--- kernel/process.c
+++ kernel/process.c
@@ -52,6 +52,21 @@ struct process {
  * Public function
  ******************************************************************************/
 
+/*
+ * Change priority of process referenced by pid to the value newprio
+ * If priority changed and the process was in a queue, it needs to be placed
+ * again in that queue depending on its new priority.
+ * If the value of newprio is invalid, return value must be < 0. Otherwise,
+ * return value is the previous priority of process
+ */
+int chprio(int pid, int newprio) { return -1; }
+
+/*
+ * If value of pid is invalid, return value must be < 0. Otherwise, return value
+ * is the current priority of process referenced by pid
+ */
+int getprio(int pid) { return -1; }
+
 /*******************************************************************************
  * Internal function
  ******************************************************************************/
