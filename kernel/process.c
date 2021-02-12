03e6b5f91eb12a132d698d6936f72f59e2a010bb
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-02-12 08:41:54 +0100

    [PROCESS] Added base structure for processes

--- kernel/process.c
+++ kernel/process.c
@@ -11,13 +11,34 @@
 
 #include "process.h"
 
+#include "stdint.h"
+
 /*******************************************************************************
  * Macros
  ******************************************************************************/
 
+// TODO make sure that kernel will support 1000 processes by the end of project
+#define NBPROC  30
+#define MAXPRIO 256
+
 /*******************************************************************************
  * Types
  ******************************************************************************/
+
+enum process_state {
+  CHOSEN, // process currently running on processor
+  READY,  // process waiting for his turn
+  BLOCKED_ON_SEMAHPORE,
+  BLOCKED_ON_IO,
+  WAITING_FOR_CHILD,
+  SLEEPING,
+  ZOMBIE
+}
+
+struct process {
+  uint32_t pid;      // between 1 and NBPROC
+  uint32_t priority; // between 1 and MAXPRIO
+};
 
 /*******************************************************************************
  * Internal function declaration
