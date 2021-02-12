a6962e15a1ccf0ad7cd9b91eb3daa659fca4a87f
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-02-12 08:41:12 +0100

    [PROCESS] Added prototypes concerning processes' priorities

--- kernel/process.h
+++ kernel/process.h
@@ -28,4 +28,20 @@
  * Prototypes
  ******************************************************************************/
 
+/*
+ * Change priority of process referenced by pid to the value newprio
+ * If priority changed and the process was in a queue, it needs to be placed
+ * again in that queue depending on its new priority.
+ * If the value of newprio is invalid, return value must be < 0. Otherwise, return
+ * value is the previous priority of process
+ */
+int chprio(int pid, int newprio);
+
+/*
+* If value of pid is invalid, return value must be < 0. Otherwise, return value
+* is the current priority of process referenced by pid
+*/
+int getprio(int pid);
+
+
 #endif /* _process_H_ */
