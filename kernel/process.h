ecc13cce408318acd6791bb5568f01b4dd809086
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-02-25 18:14:08 +0100

    Added prototypes for processes related functions Removed "TAILLE_PILE" macro as stack size is not the same for every process

--- kernel/process.h
+++ kernel/process.h
@@ -12,13 +12,16 @@
  * Includes
  ******************************************************************************/
 
+#include "stdint.h"
+
 /*******************************************************************************
  * Macros
  ******************************************************************************/
- #define TAILLE_PILE 512
- // TODO make sure that kernel will support 1000 processes by the end of project
- #define NBPROC  30
- #define MAXPRIO 256
+
+// TODO make sure that kernel will support 1000 processes by the end of project
+#define NBPROC  30
+#define MAXPRIO 256
+
 /*******************************************************************************
  * Types
  ******************************************************************************/
@@ -30,13 +33,48 @@
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-extern void ctx_sw(int saveZone1[5],int saveZone2[5]);
-int start(int (*pt_func)(void *), unsigned long ssize, int prio, const char *name, void *arg);
-int chprio(int pid, int newprio);
-int getprio(int pid);
-int tstA(void *arg);
-int tstB(void *arg);
-int tstA2();
-int tstB2();
+
+/*
+ * Create a process
+ * pt_func : main function of process
+ * ssize : size of stack
+ * prio : priority of process for execution
+ * name : name of process
+ * arg : arguments passed to the main function pt_func
+ */
+int start(int (*pt_func)(void *), unsigned long ssize, int prio,
+          const char *name, void *arg);
+
+/*
+ * Initialize the system with the main process "idle"
+ * For now, also creates two other processes A and B
+ */
+void process_init();
+
+/*
+ * Change priority of process referenced by pid to the value newprio
+ * If priority changed and the process was in a queue, it needs to be placed
+ * again in that queue depending on its new priority.
+ * If the value of newprio is invalid, return value must be < 0. Otherwise,
+ * return value is the previous priority of process
+ */
+int chprio(uint32_t pid, uint32_t newprio);
+
+/*
+ * If value of pid is invalid, return value must be < 0. Otherwise, return value
+ * is the current priority of process referenced by pid
+ */
+int getprio(uint32_t pid);
+
+/*
+ * Returns pid of calling process
+ */
+int getpid(void);
+
+/*
+ * First draft of scheduling function
+ * For now, there are only three differents processes (idle, A and B)
+ */
+void schedule();
 
 #endif /* _process_H_ */
