73d33560258e33940579883fa4622347bbc153e9
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-02-26 16:37:59 +0100

    Moved clock test from kernel_start function to its own test files Updated kernel_tests to run those tests

--- kernel/start.c
+++ kernel/start.c
@@ -1,8 +1,6 @@
 #include "stdio.h"
 #include "debugger.h"
-#include "cpu.h"
 #include "console.h"
-#include "interrupts.h"
 #include "clock.h"
 #include "process.h"
 
@@ -25,25 +23,6 @@ void kernel_start(void)
 #ifdef KERNEL_TEST
     kernel_run_tests();
 #endif
-
-  // enable interrupts and begin handler daemon
-  sti();
-  // prints elapsed time for 5 seconds
-  char time[] = "HH:MM:SS";
-  for (int i = 0; i < CLOCKFREQ*5; i++) {
-    hlt();
-
-    unsigned seconds = current_clock() / CLOCKFREQ;
-    unsigned minutes = seconds / 60;
-    seconds %= 60;
-    unsigned hours = minutes / 60;
-    minutes %= 60;
-    hours %= 24;
-    sprintf(time, "%02u:%02u:%02u", hours, minutes, seconds);
-    console_write_raw(time, 8, 24, 72);
-  }
-  // deactivate interrupts
-  cli();
 
   process_init();
 }
--- kernel/tests/test_clock.c
+++ kernel/tests/test_clock.c
@@ -1,17 +1,20 @@
 /*
- * kernel_tests.c
+ * test_clock.c
  *
- *  Created on: 11/02/2021
- *      Author: Thibault Cantori
+ *  Created on: 26/02/2021
+ *      Author: thedrin
  */
 
 /*******************************************************************************
  * Includes
  ******************************************************************************/
 
-#include "kernel_tests.h"
-#include "test_console.h"
 #include "test_clock.h"
+#include "stdio.h"
+#include "interrupts.h"
+#include "clock.h"
+#include "cpu.h"
+#include "console.h"
 
 /*******************************************************************************
  * Macros
@@ -25,6 +28,11 @@
  * Internal function declaration
  ******************************************************************************/
 
+/*
+ * Activates clock for 5 seconds
+ */
+void test_clock_count_for_five();
+
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -32,11 +40,39 @@
 /*******************************************************************************
  * Public function
  ******************************************************************************/
-void kernel_run_tests()
+
+/*
+ * Run all tests related to the clock.
+ */
+void test_clock()
 {
-  test_console();
-  test_clock();
+  test_clock_count_for_five();
 }
+
 /*******************************************************************************
  * Internal function
  ******************************************************************************/
+
+/*
+ * Activates clock for 5 seconds
+ */
+void test_clock_count_for_five()
+{
+  // enable interrupts and begin handler daemon
+  sti();
+  char time[] = "HH:MM:SS";
+  for (int i = 0; i < CLOCKFREQ * 5; i++) {
+    hlt();
+
+    unsigned seconds = current_clock() / CLOCKFREQ;
+    unsigned minutes = seconds / 60;
+    seconds %= 60;
+    unsigned hours = minutes / 60;
+    minutes %= 60;
+    hours %= 24;
+    sprintf(time, "%02u:%02u:%02u", hours, minutes, seconds);
+    console_write_raw(time, 8, 24, 72);
+  }
+  // deactivate interrupts
+  cli();
+}
