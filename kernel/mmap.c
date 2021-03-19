e2db704304b647ff22b138f73526990f8dd2cfd1
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-03-19 04:00:19 +0100

    Implement manual address translation

--- kernel/mmap.c
+++ kernel/mmap.c
@@ -20,8 +20,10 @@
 
 #define TOTAL_MEMORY  0x10000000 /* 256 MiB */
 #define TOTAL_PAGES   (TOTAL_MEMORY / PAGE_SIZE)
+
 #define KERNEL_MEMORY 0x04000000 /* 64 MiB */
 #define KERNEL_PAGES  (KERNEL_MEMORY / PAGE_SIZE)
+
 #define POOL_SIZE     (TOTAL_PAGES - KERNEL_PAGES) /* page units */
 
 /*******************************************************************************
@@ -41,40 +43,67 @@ struct page {
  ******************************************************************************/
 
 // Free-list -based pool allocator for physical memory pages.
-static struct page  g_page_pool[POOL_SIZE];
+static struct page  g_memory[POOL_SIZE];
 static struct page *g_free_list;
 
 /*******************************************************************************
  * Public function
  ******************************************************************************/
 
-void page_init(void)
+// Reference: https://wiki.osdev.org/Paging
+void page_map(uint32_t *pgdir, const void *virt, const void *real, int flags)
+{
+  // check for page alignment
+  assert(((uint32_t)virt & (PAGE_SIZE-1)) == 0);
+  assert(((uint32_t)real & (PAGE_SIZE-1)) == 0);
+
+  // get the page dir entry and check if the associated page table is present
+  const unsigned pd_index = ((uint32_t)virt >> 22) & 0x3FF;
+  const uint32_t pd_entry = pgdir[pd_index];
+  if (!(pd_entry & PAGE_PRESENT)) {
+    // TODO: allocate missing page table
+  }
+
+  // get the page table entry and see it there's already a mapping present
+  uint32_t * const pgtab = (uint32_t *)(pd_entry & 0xFFFFF000);
+  const unsigned pt_index = ((uint32_t)virt >> 12) & 0x3FF;
+  const uint32_t pt_entry = pgtab[pt_index];
+  if (pt_entry & PAGE_PRESENT) {
+    // TODO: resolve page mapping conflict
+  }
+
+  // setup address translation, applying flags and marking target as present
+  pgtab[pt_index] = (uint32_t)real | (flags & 0xFFF) | PAGE_PRESENT;
+  // TODO: do we need to flush some TLB entry?
+}
+
+void frame_init(void)
 {
   // add all pages in the pool (w/ kernel frames excluded) to the free list
   g_free_list = NULL;
   for (unsigned i = 0; i < POOL_SIZE; ++i) {
-    g_page_pool[i].next = g_free_list;
-    g_free_list = &g_page_pool[i];
+    g_memory[i].next = g_free_list;
+    g_free_list = &g_memory[i];
   }
 }
 
-int page_alloc(void)
+int frame_alloc(void)
 {
   // pop free list head and use its address to compute index and frame number
   if (g_free_list == NULL) return -1;
-  const unsigned i = g_free_list - g_page_pool;
+  const unsigned i = g_free_list - g_memory;
   g_free_list = g_free_list->next;
   return KERNEL_PAGES + i;
 }
 
-void page_free(int frame)
+void frame_free(int frame)
 {
   // get pool index from frame number
   const int i = frame - KERNEL_PAGES;
   assert(i >= 0 && i < POOL_SIZE);
   // add page to free list
-  g_page_pool[i].next = g_free_list;
-  g_free_list = &g_page_pool[i];
+  g_memory[i].next = g_free_list;
+  g_free_list = &g_memory[i];
 }
 
 /*******************************************************************************
--- kernel/mmap.h
+++ kernel/mmap.h
@@ -12,11 +12,16 @@
  * Includes
  ******************************************************************************/
 
+#include "stdint.h"
+
 /*******************************************************************************
  * Macros
  ******************************************************************************/
 
 #define PAGE_SIZE 0x1000 /* 4 KiB */
+
+/// Page flags.
+#define PAGE_PRESENT 0x01
 
 /*******************************************************************************
  * Types
@@ -31,18 +36,24 @@
  ******************************************************************************/
 
 /**
+ * Creates a virtual-physical address mapping in the PGDIR page directory.
+ * Addresses VIRT and REAL must be page-aligned and the given FLAGS will apply.
+ */
+void page_map(uint32_t *pgdir, const void *virt, const void *real, int flags);
+
+/**
  * Initializes the physical page allocator system.
  * Considers kernel-reserved frames as being in use.
  */
-void page_init(void);
+void frame_init(void);
 
 /**
- * Acquires an unused physical memory page of PAGE_SIZE bytes.
+ * Acquires an unused page frame of exactly PAGE_SIZE bytes.
  * Returns a frame index on success and a negative value when out of memory.
  */
-int page_alloc(void);
+int frame_alloc(void);
 
-/// Releases a physical memory page with a certain frame index.
-void page_free(int frame);
+/// Releases a page frame with a certain index.
+void frame_free(int frame);
 
 #endif // _MMAP_H_
