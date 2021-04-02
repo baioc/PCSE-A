271da1d93e78ecb11f9648d9d6b486e308693965
briancan <antoine.briancon@grenoble-inp.org>
2021-04-02 09:51:55 +0200

    Mise en place test 11 semaphores (pas validé)

--- kernel/tests/userspace_tests.c
+++ kernel/tests/userspace_tests.c
@@ -15,9 +15,9 @@
 #include "debug.h"
 #include "shared_memory.h"
 #include "sem.h"
-
 #include "test1.h"
 #include "test4.h"
+#include "test11.h"
 #include "test13.h"
 
 /*******************************************************************************
@@ -62,6 +62,9 @@ static unsigned long long div64(unsigned long long x, unsigned long long div,
                                 unsigned long long *rem);*/
 
 static int test_10_sem(void *arg);
+
+static int test_11_sem(void *arg);
+static int proc_mutex(void *arg);
 
 static int test_12_msg(void *arg);
 static int rdv_proc_12_msg(void *arg);
@@ -586,7 +589,101 @@ static int waiter(void *arg)
 /*-----------------*
  *      Test 11
  *-----------------*/
-// TODO: Add test_11 when semaphore and shared memory are available
+
+ void xwait(union sem *s)
+ {
+         assert(wait(s->sem) == 0);
+ }
+
+ void xsignal(union sem *s)
+ {
+         assert(signal(s->sem) == 0);
+ }
+
+ void xscreate(union sem *s)
+ {
+         assert((s->sem = screate(0)) >= 0);
+ }
+
+ void xsdelete(union sem *s)
+ {
+         assert(sdelete(s->sem) == 0);
+ }
+
+ static int test_11_sem(void *arg)
+ {
+         struct test11_shared *shared = NULL;
+         int pid1, pid2, pid3, pid4;
+
+         (void)arg;
+         shared = (struct test11_shared*) shm_create("test11_shm");
+         assert(shared != NULL);
+         assert(getprio(getpid()) == 128);
+         xscreate(&shared->sem);
+         shared->in_mutex = 0;
+         printf("1");
+
+         pid1 = start(proc_mutex, 4000, 130, "proc_mutex", 0);
+         pid2 = start(proc_mutex, 4000, 132, "proc_mutex", 0);
+         pid3 = start(proc_mutex, 4000, 131, "proc_mutex", 0);
+         pid4 = start(proc_mutex, 4000, 129, "proc_mutex", 0);
+         assert(pid1 > 0);
+         assert(pid2 > 0);
+         assert(pid3 > 0);
+         assert(pid4 > 0);
+         assert(chprio(getpid(), 160) == 128);
+         printf(" 6");
+         xsignal(&shared->sem);
+         assert(waitpid(-1, 0) == pid2);
+         assert(waitpid(-1, 0) == pid3);
+         assert(waitpid(-1, 0) == pid1);
+         assert(waitpid(-1, 0) == pid4);
+         assert(waitpid(-1, 0) < 0);
+         assert(chprio(getpid(), 128) == 160);
+         xsdelete(&shared->sem);
+         printf(" 11.\n");
+
+         return 0;
+ }
+
+ static int proc_mutex(void *arg)
+ {
+         struct test11_shared *shared = NULL;
+         int p = -1;
+         int msg;
+
+         (void)arg;
+         shared = (struct test11_shared*) shm_acquire("test11_shm");
+         assert(shared != NULL);
+         p = getprio(getpid());
+         assert(p > 0);
+
+         switch (p) {
+                 case 130:
+                         msg = 2;
+                         break;
+                 case 132:
+                         msg = 3;
+                         break;
+                 case 131:
+                         msg = 4;
+                         break;
+                 case 129:
+                         msg = 5;
+                         break;
+                 default:
+                         msg = 15;
+         }
+         printf(" %d", msg);
+         xwait(&shared->sem);
+         printf(" %d", 139 - p);
+         assert(!(shared->in_mutex++));
+         chprio(getpid(), 16);
+         chprio(getpid(), p);
+         shared->in_mutex--;
+         xsignal(&shared->sem);
+         return 0;
+ }
 
 /*-----------------*
  *      Test 12
