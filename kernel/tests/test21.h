03e5779ff06b1fe93753a659184fef958e1292ba
Thibault Cantori <thibault.cantori@grenoble-inp.org>
2021-04-08 14:54:26 +0200

    Forgot header file in last commit relative to test_21...

---
+++ kernel/tests/test21.h
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Ensimag - Projet Systeme
+ * Copyright 2013 - Damien Dejean <dam.dejean@gmail.com>
+ *
+ * Test 21 : Common definitions
+ *******************************************************************************/
+#ifndef _TEST21_H_
+#define _TEST21_H_
+
+#define FILL_VALUE          0xCA
+#define CHECKER_SUCCESS     0x12345678
+
+#endif /* _TEST21_H_ */
